4823c10b3f7909e61e0a0f75f80c0fbdf6d73f03
Dmitriy <drumkq@bk.ru>
2023-02-13 11:36:12 +0300

    Initial commit

---
+++ src/MyClass.cpp
@@ -0,0 +1,9 @@
+#include "MyClass.hpp"
+
+namespace Nitreet
+{
+    int MyClass::foo(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ src/MyClass.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Nitreet
+{
+    class MyClass
+    {
+    public:
+        static int foo(int a, int b);
+    };
+}
